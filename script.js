8fb2e0a9b79ef1d9be6affeecee7dfc1ad3baf72
Ameya Kawade <ameyakawade0@gmail.com>
2024-02-07 21:14:23 +0530

    modified script.js

--- script.js
+++ script.js
@@ -1,25 +1,233 @@
-const taskInput=document.querySelector("#task"),dateTimeInput=document.querySelector("#datetime"),taskBtn=document.querySelector("#add-task-btn"),taskList=document.querySelector("#tasks-list"),bellBtn=document.querySelector("#bell-btn");let currDateTime=new Date,taskCount=0,tasks=localStorage.getItem("tasks"),notificationPermission=!1;function createNotification(t){let e=new Date(t.timing)-new Date;if(console.log(e),""===t.status){if(e<Number.MAX_SAFE_INTEGER)return"granted"===Notification.permission?(console.log("Creating notification"),setTimeout(()=>{new Notification("Todo List",{body:`${t.task}
-Status : ${e>0?"pending":"overdue"}
-Date: ${t.date}-${t.month}-${t.year}
-Time : ${t.hour} : ${t.minutes} ${t.amPm}`,icon:"../images/icon.png"})},e)):alert("Cannot create notifications since the notification permission is denied, to get notifications please grant notification permission."),!0;alert("Cannot create notification, please select a date closer to current date")}return!1}function returnTasksString(t,e){return`<div class="tasks-design ${e[t].status}" id = ${t}>
+const taskInput = document.querySelector("#task");
+const dateTimeInput = document.querySelector("#datetime");
+const taskBtn = document.querySelector("#add-task-btn");
+const taskList = document.querySelector("#tasks-list");
+const bellBtn = document.querySelector("#bell-btn");
+let currDateTime = new Date();
+let taskCount = 0;
+let tasks = localStorage.getItem("tasks");
+let notificationPermission = false;
+
+
+function createNotification(task){
+    let time = new Date(task['timing']) - new Date();
+    console.log(time);
+    if(task['status'] === ""){
+        if(time < Number.MAX_SAFE_INTEGER){
+            if(Notification.permission === "granted"){
+                console.log("Creating notification");
+                setTimeout(()=>{
+                    new Notification("Todo List",{
+                        body:`${task['task']}\nStatus : ${(time > 0)?'pending':'overdue'}\nDate: ${task['date']}-${task['month']}-${task['year']}\nTime : ${task['hour']} : ${task['minutes']} ${task['amPm']}`,
+                        icon:"../images/icon.png"
+                    });
+                },time);
+            }
+            else{
+                alert("Cannot create notifications since the notification permission is denied, to get notifications please grant notification permission.")
+            }
+            return true;
+        }
+        else{
+            alert("Cannot create notification, please select a date closer to current date");
+        }
+    }
+    return false;
+}
+
+function returnTasksString(i,taskArray){
+    return `<div class="tasks-design ${taskArray[i].status}" id = ${i}>
                     <div class="task-container">
                         <p>
                             <span class="font-semibold text-lg">Task:</span>
                             <p class="text-justify">
-                                ${e[t].task}
+                                ${taskArray[i].task}
                             </p>
                         </p>
                         <p>
                             <span class="font-semibold text-lg">Date:</span><br>
-                            ${e[t].date} - ${e[t].month} - ${e[t].year}
+                            ${taskArray[i].date} - ${taskArray[i].month} - ${taskArray[i].year}
                         </p>
                         <p>
                             <span class="font-semibold text-lg">Time:</span> <br>
-                            ${e[t].hour} : ${e[t].minutes} ${e[t].amPm}
+                            ${taskArray[i].hour} : ${taskArray[i].minutes} ${taskArray[i].amPm}
                         </p>
                     </div>
                     <div class="task-btn-frame">
-                        <button id = done-btn${t} class="bg-green-500 task-btn hover:bg-green-400">${"completed"===e[t].status?"Undone":"Done"}</button>
-                        <button id = delete-btn${t} class="task-btn bg-red-500 hover:bg-red-400">Delete</button>
+                        <button id = done-btn${i} class="bg-green-500 task-btn hover:bg-green-400">${(taskArray[i].status === "completed")?"Undone":"Done"}</button>
+                        <button id = delete-btn${i} class="task-btn bg-red-500 hover:bg-red-400">Delete</button>
                     </div>
-                </div>`}const doneBtnFunc=t=>{let e=t.target.parentNode.parentNode;if(console.log(e),console.log(t),"Undone"===t.target.textContent)t.target.textContent="Done",tasks[Number(e.getAttribute("id"))].status="";else{t.target.textContent="Undone";tasks[Number(e.getAttribute("id"))].status="completed"}e.classList.toggle("completed"),localStorage.setItem("tasks",JSON.stringify(tasks))},deleteBtnFunc=t=>{console.log("Delete btn clicked");let e=Number(t.target.parentNode.parentNode.getAttribute("id"));tasks.splice(e,1),console.log(tasks),document.getElementById(`${e}`).classList.add("animate-fade-out"),setTimeout(()=>{document.getElementById(`${e}`).remove(),addEventListenersToElements(tasks.length,!1),renderTasks(tasks),addEventListenersToElements(tasks.length-1,!0),localStorage.setItem("tasks",JSON.stringify(tasks))},1200)};function addEventListenersToElements(t,e){if(!0===e)for(let n=0;n<=t;n++){let s=document.querySelector(`#done-btn${n}`);s&&(s.addEventListener("click",doneBtnFunc),document.querySelector(`#delete-btn${n}`).addEventListener("click",deleteBtnFunc))}else for(let a=0;a<=t;a++){let i=document.querySelector(`#done-btn${a}`);i&&(i.removeEventListener("click",doneBtnFunc),document.querySelector(`#delete-btn${a}`).removeEventListener("click",deleteBtnFunc))}}function addEventListenerToDoneBtn(t){t.addEventListener("click",doneBtnFunc),console.log("Added")}function addEventListenerToDeleteBtn(t){t.addEventListener("click",deleteBtnFunc),console.log("Added")}function returnFragment(t){return document.createRange().createContextualFragment(t)}function renderTasks(t){let e=new DocumentFragment,n=t.length,s;for(s=0;s<n;s++){let a=returnFragment(returnTasksString(s,t));e.append(a)}taskCount=s-1,taskList.textContent="",taskList.appendChild(e)}function renderTask(t,e){let n=new DocumentFragment;if(n.append(returnFragment(returnTasksString(t,e))),n.firstChild){let s=n.firstChild;s.classList.add("animate-fade-in"),setTimeout(()=>{s.classList.remove("animate-fade-in")},1100)}else console.log("No first child");taskList.append(n)}function initialization(){if(tasks)for(let t of(tasks=JSON.parse(tasks),renderTasks(tasks),addEventListenersToElements(tasks.length-1,!0),tasks))createNotification(t);else tasks=[];let e=new Date;e.setUTCHours(e.getUTCHours()+5.5,e.getUTCMinutes(),0,0),dateTimeInput.min=e.toISOString().slice(0,16),dateTimeInput.value=e.toISOString().slice(0,16)}taskBtn.addEventListener("click",()=>{let t=taskInput.value,e=dateTimeInput.value;if(t&&e){let n=(e=new Date(e)).getFullYear().toString(),s=(e.getMonth()+1).toString().padStart(2,"0"),a=e.getDate().toString().padStart(2,"0"),i=e.getHours(),o=(i%12||12).toString().padStart(2,"0"),r,d={task:t,year:n,date:a,month:s,hour:o,amPm:i<12?"am":"pm",minutes:e.getMinutes().toString().padStart(2,"0"),status:"",timing:e};createNotification(d)&&(tasks.push(d),renderTask(taskCount=tasks.length-1,tasks),addEventListenerToDoneBtn(document.querySelector(`#done-btn${taskCount}`)),addEventListenerToDeleteBtn(document.querySelector(`#delete-btn${taskCount}`)),localStorage.setItem("tasks",JSON.stringify(tasks)),console.log(e-new Date))}}),bellBtn.addEventListener("click",t=>{"granted"!==Notification.permission&&"denied"!==Notification.permission?Notification.requestPermission().then(t=>{}):"granted"===Notification.permission?alert("Notifications are enabled already."):alert("Cannot send notifications since you have denied the permission to send notifications.")}),initialization();
+                </div>`;
+}
+
+
+const doneBtnFunc = (e)=>{
+    let parent = e.target.parentNode.parentNode;
+    console.log(parent);
+    console.log(e);
+    if(e.target.textContent === "Undone"){
+        e.target.textContent = "Done"
+        tasks[Number(parent.getAttribute("id"))].status = "";
+    }
+    else{
+        e.target.textContent = "Undone";
+        let idx = Number(parent.getAttribute("id"));
+        tasks[idx].status = "completed";
+    }
+    parent.classList.toggle("completed");
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+};
+
+const deleteBtnFunc = (e)=>{
+    console.log("Delete btn clicked");
+    let taskId =  Number(e.target.parentNode.parentNode.getAttribute("id"));
+    tasks.splice(taskId,1);
+    console.log(tasks);
+    document.getElementById(`${taskId}`).classList.add("animate-fade-out");
+    setTimeout(()=>{
+        document.getElementById(`${taskId}`).remove();
+        addEventListenersToElements(tasks.length,false);
+        renderTasks(tasks);
+        addEventListenersToElements(tasks.length - 1, true);
+        localStorage.setItem("tasks",JSON.stringify(tasks));
+    },1200);
+};
+
+
+function addEventListenersToElements(length, add){
+    if (add === true){
+        for(let i = 0; i <= length; i++){
+            let el = document.querySelector(`#done-btn${i}`);
+            if(el){
+                el.addEventListener("click",doneBtnFunc);
+                document.querySelector(`#delete-btn${i}`).addEventListener("click",deleteBtnFunc);
+            }
+        }
+    }
+    else{
+        for(let i = 0; i <= length; i++){
+            let el = document.querySelector(`#done-btn${i}`);
+            if (el){
+                el.removeEventListener("click",doneBtnFunc);
+                document.querySelector(`#delete-btn${i}`).removeEventListener("click",deleteBtnFunc);
+            }
+        }
+    }
+}
+
+function addEventListenerToDoneBtn(element){
+    element.addEventListener("click",doneBtnFunc);
+    console.log("Added");
+}
+function addEventListenerToDeleteBtn(element){
+    element.addEventListener("click",deleteBtnFunc);
+    console.log("Added");
+}
+
+function returnFragment(content){
+    let range = document.createRange();
+    let fragment = range.createContextualFragment(content);
+    return fragment;
+}
+
+function renderTasks(taskArray){
+    let docfrag = new DocumentFragment();
+    let l = taskArray.length;
+    let i;
+    for(i = 0; i < l; i++){
+        let fragment = returnFragment(returnTasksString(i,taskArray));
+        docfrag.append(fragment);
+    }
+    taskCount = i - 1;
+    taskList.textContent = "";
+    taskList.appendChild(docfrag);
+}
+
+function renderTask(i,taskArray){
+    let docfrag = new DocumentFragment();
+    docfrag.append(returnFragment(returnTasksString(i,taskArray)));
+    if(docfrag.firstChild){
+        let el = docfrag.firstChild;
+        el.classList.add("animate-fade-in");
+        setTimeout(()=>{
+            el.classList.remove("animate-fade-in");
+        },1100);
+    }
+    else{
+        console.log("No first child");
+    }
+    taskList.append(docfrag);
+}
+
+taskBtn.addEventListener("click",()=>{
+    let task = taskInput.value;
+    let datetime = dateTimeInput.value;
+    if(task && datetime){
+        datetime = new Date(datetime);
+        let year = datetime.getFullYear().toString();
+        let month = (datetime.getMonth() + 1).toString().padStart(2,"0");
+        let date = datetime.getDate().toString().padStart(2,"0");
+        let h = datetime.getHours();
+        let hour = ((h) % 12 || 12).toString().padStart(2,"0");
+        let amPm = (h < 12)?"am":"pm";
+        let minutes = (datetime.getMinutes()).toString().padStart(2,"0");
+        let taskObj = {
+            "task" : task,
+            "year" : year,
+            "date" : date,
+            "month" : month,
+            "hour" : hour,
+            "amPm" : amPm,
+            "minutes" : minutes,
+            "status":"",
+            "timing":datetime
+        };
+        if(createNotification(taskObj)){
+
+            tasks.push(taskObj);
+            taskCount = tasks.length - 1;
+            renderTask(taskCount,tasks);
+            addEventListenerToDoneBtn(document.querySelector(`#done-btn${taskCount}`));
+            addEventListenerToDeleteBtn(document.querySelector(`#delete-btn${taskCount}`));
+            localStorage.setItem("tasks",JSON.stringify(tasks));
+            console.log(datetime - new Date());
+            
+        }
+    }
+});
+
+bellBtn.addEventListener("click",(e)=>{
+
+    if(Notification.permission !== "granted" && Notification.permission !== "denied"){
+        Notification.requestPermission().then((permission)=>{
+        });
+    }
+    else if(Notification.permission === "granted"){
+        alert("Notifications are enabled already.")
+    }
+    else{
+        alert("Cannot send notifications since you have denied the permission to send notifications.");
+    }
+});
+
+
+function initialization(){
+    if (tasks){
+        tasks = JSON.parse(tasks);
+        renderTasks(tasks);
+        addEventListenersToElements(tasks.length - 1,true);
+        for(let task of tasks){
+            createNotification(task);
+        }
+    }
+    else{
+    tasks = [];
+    }
+
+    const utcDate = new Date();
+    utcDate.setUTCHours(utcDate.getUTCHours() + 5.5, utcDate.getUTCMinutes()+30, 0, 0); // Ensure 0 seconds and milliseconds for consistency
+    dateTimeInput.min = utcDate.toISOString().slice(0, 16);
+    dateTimeInput.value = utcDate.toISOString().slice(0, 16);
+}
+
+initialization();
